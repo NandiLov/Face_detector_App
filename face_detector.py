438d5207bac96ce060a348f1baf1c062110d9baa
NandiLov <89518874+NandiLov@users.noreply.github.com>
2023-02-19 18:12:28 +0200

    first commit

---
+++ face_detector.py
@@ -0,0 +1,32 @@
+import cv2
+
+# Load the Haar cascade file
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+# Get the webcam
+cap = cv2.VideoCapture(0)
+
+while True:
+    # Read the frame
+    _, img = cap.read()
+
+    # Convert to grayscale
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    # Detect the faces
+    faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+
+    # Draw the rectangle around each face
+    for (x, y, w, h) in faces:
+        cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
+
+    # Show the image
+    cv2.imshow('img', img)
+
+    # Stop if escape key is pressed
+    k = cv2.waitKey(30) & 0xff
+    if k==27:
+        break
+
+# Release the VideoCapture object
+cap.release()
